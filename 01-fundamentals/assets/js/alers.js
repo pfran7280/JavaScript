72392c2468fc5b39b91e7af47dfb584b77544d69
pfran7280 <pfran7280@gmail.com>
2023-06-22 20:38:53 +0200

    01-fundamentals

---
+++ 01-fundamentals/assets/js/alers.js
@@ -0,0 +1,8 @@
+// console.log(miName);
+
+// let nombre = prompt('That's your name');
+
+// console.log('nombre');
+
+const selected = confirm('Are you sure about delete it');
+console.log()
---
+++ 01-fundamentals/assets/js/app.js
@@ -0,0 +1,32 @@
+
+
+
+// console.log('Hello world');
+
+// let a = 10;
+// var b = 10;
+// const c = 10;
+
+let a = 10,
+    b = 29,  
+    c = 'Hello',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+// console.log('%c Mis variables', 'color:blue; font-weight:bold');
+// console.log({a});
+// console.log({b});
+// console.log({c});
+
+console.table([a, b, c, d, x]);
+
+c = 'Hola de nuevo'
+
+// var miNombre = 'Franklin';
+
+var outerWidth = 1000000;
+
+let outerHeight = 1000000;
+
